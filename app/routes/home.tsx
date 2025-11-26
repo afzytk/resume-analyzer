4bbb022fd1a290decc21ab2c8247af511e00b6d8
afzytk <afzytk@gmail.com>
2025-11-26 17:03:39 +0530

    feat(home): add background image and update page meta

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home"
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className='main-section'>
+        <div className='page-heading'>
+            <h1>Track Your applications & Resume Rating</h1>
+            <h2>Review your submission and check AI-Powered feedback</h2>
+        </div>
+    </section>
+      </main>;
 }
